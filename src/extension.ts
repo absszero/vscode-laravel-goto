ce6c61d6014487d3113629dd66317e3c29b51186
absszero <absszero@gmail.com>
2019-08-30 20:22:25 +0800

    search view file and controller

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let editor: vscode.TextEditor;
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
+	let disposable = vscode.commands.registerTextEditorCommand('extension.vscode-laravel-goto',
+	(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any[]) => {
+		editor = textEditor;
+		let selection = getSelection(editor.selection);
+		let path = getPath(selection);
+		search(path);
 	});
 
 	context.subscriptions.push(disposable);
@@ -25,3 +15,66 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function search(path: string)
+{
+	if (isController(path)) {
+		path = path + '.php';
+	} else {
+		path = path.replace(/\./g, '/') + '.blade.php';
+	}
+
+	vscode.commands.executeCommand('workbench.action.quickOpen', path);
+}
+
+function getPath(selection: vscode.Range)
+{
+	let path = editor.document.getText(selection);
+	if (isController(path))
+	{
+		// namespace = self.get_namespace(selection)
+		// if namespace:
+		path = path;
+	} else {
+		let splited = path.split(':');
+        path = splited[splited.length - 1];
+	}
+
+	return path
+}
+
+function isController(path: string)
+{
+	return (-1 !== path.indexOf('@') || -1 !== path.indexOf('Controller'));
+}
+
+function getSelection(selected: vscode.Selection) {
+	let start = selected.start;
+	let end = selected.end;
+
+	if (!start.isEqual(end)) {
+		return new vscode.Range(start, end);
+	}
+
+	const line = editor.document.lineAt(start);
+	const DELIMITERS = "\"'"
+	while (start.isAfter(line.range.start)) {
+		let next = start.with({character: start.character - 1})
+		let char = editor.document.getText(new vscode.Range(next, start));
+		if (-1 !== DELIMITERS.indexOf(char)) {
+			break;
+		}
+		start = next;
+	}
+	while (end.isBefore(line.range.end)) {
+		let next = end.with({character: end.character + 1})
+		let char = editor.document.getText(new vscode.Range(end, next));
+		if (-1 !== DELIMITERS.indexOf(char)) {
+			break;
+		}
+		end = next;
+	}
+
+	return new vscode.Range(start, end);
+}
+
