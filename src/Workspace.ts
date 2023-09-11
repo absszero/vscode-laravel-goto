91b10af6ee33392074169b5a2b015b8029af0b45
Adrian Chen <absszero@gmail.com>
2023-09-11 17:41:17 +0800

    add reading from cache

--- src/Workspace.ts
+++ src/Workspace.ts
@@ -3,7 +3,8 @@ import {promises as fsp} from "fs";
 
 const MAX_RESULTS = 2;
 const excludes = vscode.workspace.getConfiguration().get('laravelGoto.exclusion', null);
-
+let contents = new Map<string, string>;
+let mTimes  = new Map<string, string>;
 /**
  * locate files
  *
@@ -21,10 +22,22 @@ export async function findFiles(glob: string, maxResults = MAX_RESULTS) {
  */
 export async function getFileContent(pathGlob: string): Promise<string> {
     const uri = await findFiles('**/' + pathGlob, 1);
-    let content = '';
-    if (uri.length) {
-        content = (await fsp.readFile(uri[0].path)).toString();
+    if (0 === uri.length) {
+        return '';
     }
+    const filepath = uri[0].path;
+
+    // read from cache
+    const mTime = (await fsp.stat(filepath)).mtime.toString();
+    if (mTimes.get(filepath) === mTime) {
+        return contents.get(filepath) || '';
+
+    }
+
+    // read from disk
+    const content = (await fsp.readFile(filepath)).toString();
+    mTimes.set(filepath, mTime);
+    contents.set(filepath, content);
 
     return content;
 }
