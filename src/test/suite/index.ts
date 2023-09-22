57272fb789ea055898130896b23461da94a5b926
Adrian Chen <absszero@gmail.com>
2023-09-22 10:15:55 +0800

    add running test on current test case

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 import * as fs from 'fs';
+import * as vscode from 'vscode';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -12,22 +13,34 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
+	return new Promise(async (c, e) => {
 		let filePattern = '**/**.test.js';
 
 		if (process.env.TEST_FILE) {
 			const prevousFilename = process.env.EXTENSION_PATH + '/.vscode-test/.prevous';
+			let lineNumber = '';
 			if (process.env.TEST_FILE.endsWith('test.ts')) {
 				filePattern = process.env.TEST_FILE;
-				filePattern = filePattern.replace('src/test/', '');
-				filePattern = filePattern.replace('.ts', '.js');
+				if (process.env.TEST_FILE_LINE) {
+					lineNumber = process.env.TEST_FILE_LINE;
+				}
 
-				fs.writeFile(prevousFilename, filePattern, err => null);
+				fs.writeFile(prevousFilename, filePattern + ':' + lineNumber, err => null);
 			} else {
 				if (fs.existsSync(prevousFilename) ) {
-					filePattern = fs.readFileSync(prevousFilename, 'utf8');
+					const content = fs.readFileSync(prevousFilename, 'utf8').split(':');
+					filePattern = content[0];
+					lineNumber = content[1];
 				}
 			}
+
+			const testCase = await getTestCase(filePattern, +lineNumber);
+			if (testCase) {
+				mocha.grep(testCase);
+			}
+			filePattern = filePattern.replace('src/test/', 'out/test');
+			filePattern = filePattern.replace('.ts', '.js');
+			filePattern = filePattern.replace(testsRoot, '');
 		}
 
 		glob(filePattern, { cwd: testsRoot }, (err, files) => {
@@ -52,4 +65,18 @@ export function run(): Promise<void> {
 			}
 		});
 	});
+}
+
+async function getTestCase(filename: string, lineNumber: number) : Promise<string> {
+	const document = await vscode.workspace.openTextDocument(filename);
+	while (lineNumber > 0) {
+		const lineText = document.lineAt(lineNumber).text;
+		const match = lineText.match(/test\(\s*['"`]([^'"`]+)/);
+		if (match) {
+			return match[1];
+		}
+		--lineNumber;
+	}
+
+	return '';
 }
