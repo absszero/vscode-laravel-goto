4adb488e0d5f452834facaa2bff56fa3a37dd936
Adrian Chen <absszero@gmail.com>
2023-10-18 11:04:47 +0800

    fix test

--- src/test/suite/Console.test.ts
+++ src/test/suite/Console.test.ts
@@ -4,6 +4,7 @@ import * as assert from 'assert';
 import { Console } from '../../Console';
 import { afterEach } from 'mocha';
 import * as workspace from '../../Workspace';
+import { normalize } from 'path';
 
 suite('Console Test Suite', () => {
 	afterEach(() => {
@@ -12,12 +13,13 @@ suite('Console Test Suite', () => {
 
 	test('all', async () => {
 		const subFindFiles = sinon.stub(workspace, 'findFiles');
-		const kernel = Uri.file(__dirname + '/../../../src/test/test-fixtures/app/Console/Kernel.php');
+		const path = '/../../../src/test/test-fixtures/app/Console/';
+		const kernel = Uri.file(normalize(__dirname + path + 'Kernel.php'));
 		const files = [
-			Uri.file(__dirname + '/../../../src/test/test-fixtures/app/Console/Commands/SayHello.php'),
-			Uri.file(__dirname + '/../../../src/test/test-fixtures/app/Console/Commands/Subdir/SendEmails.php'),
+			Uri.file(normalize(__dirname + path + 'Commands/SayHello.php')),
+			Uri.file(normalize(__dirname + path + 'Commands/Subdir/SendEmails.php')),
 		];
-		const registerCmd = Uri.file(__dirname + '/../../../src/test/test-fixtures/app/Console/Commands/SayGoodbye.php');
+		const registerCmd = Uri.file(normalize(__dirname + path + 'Commands/SayGoodbye.php'));
 		subFindFiles.onCall(0).returns(new Promise((resolve) => resolve([kernel])));
 		subFindFiles.onCall(1).returns(new Promise((resolve) => resolve(files)));
 		subFindFiles.onCall(2).returns(new Promise((resolve) => resolve([registerCmd])));
