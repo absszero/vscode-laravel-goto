9027eb4257102e801bc5a624b1ab0a995895a7b3
Adrian Chen <absszero@gmail.com>
2024-02-20 10:23:24 +0800

    refactor

--- src/Blade.ts
+++ src/Blade.ts
@@ -20,31 +20,10 @@ export class Blade {
 			/(resources\/views[^\s'"-]+)/,
 		];
 
-		const transformFilename = (place: Place) => {
-			const split = path.split(':');
-			let vendor = '';
-			// namespace or vendor
-			if (3 === split.length) {
-				// it's vendor
-				if (split[0] === split[0].toLowerCase()) {
-					vendor = split[0] + '/';
-				}
-			}
-
-			place.path = split[split.length - 1];
-			place.path = vendor + place.path.replace(/\./g, '/');
-			if (place.path.endsWith('/blade/php')) {
-				place.path = place.path.slice(0, place.path.length - '/blade/php'.length);
-			}
-			place.path += '.blade.php';
-
-			return place;
-		};
-
 		for (const pattern of patterns) {
 			const match = pattern.exec(line) ?? pattern.exec(lines);
 			if (match && match[match.length - 1] === path) {
-				place = transformFilename(place);
+				place = this.transformFilename(path, place);
 
 				return place;
 			}
@@ -59,10 +38,38 @@ export class Blade {
 
 		for (const pattern of multiViewsPatterns) {
 			if (pattern.exec(line) ?? pattern.exec(lines)) {
-				place = transformFilename(place);
+				place = this.transformFilename(path, place);
 				return place;
 			}
 		}
+
+		return place;
+	}
+
+	/**
+	 *
+	 * @param path
+	 * @param place
+	 * @returns Place
+	 */
+	protected transformFilename(path: string, place: Place): Place
+	{
+		const split = path.split(':');
+		let vendor = '';
+		// namespace or vendor
+		if (3 === split.length) {
+			// it's vendor
+			if (split[0] === split[0].toLowerCase()) {
+				vendor = split[0] + '/';
+			}
+		}
+
+		place.path = split[split.length - 1];
+		place.path = vendor + place.path.replace(/\./g, '/');
+		if (place.path.endsWith('/blade/php')) {
+			place.path = place.path.slice(0, place.path.length - '/blade/php'.length);
+		}
+		place.path += '.blade.php';
 
 		return place;
 	}
