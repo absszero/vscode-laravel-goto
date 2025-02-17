4652d0e8f27ffb204652cb169bda6aa1b7a5bd30
Adrian Chen <absszero@gmail.com>
2025-02-17 20:05:34 +0800

    refactor RouteItem for consistent formatting and indentation

--- src/RouteItem.ts
+++ src/RouteItem.ts
@@ -5,22 +5,22 @@ import { IRouteRow } from './IRouteRow';
 export class RouteItem implements QuickPickItem {
 
 	label: string;
-  description?: string;
+	description?: string;
 	detail?: string;
-  place?: Place;
-  controller: string;
-  kind?: QuickPickItemKind;
+	place?: Place;
+	controller: string;
+	kind?: QuickPickItemKind;
 
-	constructor(public route : IRouteRow | undefined = undefined, public thePlace: Place | undefined = undefined) {
-    this.label = '';
-    this.controller = '';
-    if (route) {
-      this.label = route.uri;
-      this.description = route.method;
-      this.detail = route.action;
-      this.controller = route.action.split('@')[0];
-    }
-    this.place = thePlace;
-    this.kind = QuickPickItemKind.Default;
+	constructor(public route: IRouteRow | undefined = undefined, public thePlace: Place | undefined = undefined) {
+		this.label = '';
+		this.controller = '';
+		if (route) {
+			this.label = route.uri;
+			this.description = route.method;
+			this.detail = route.action;
+			this.controller = route.action.split('@')[0];
+		}
+		this.place = thePlace;
+		this.kind = QuickPickItemKind.Default;
 	}
 }
